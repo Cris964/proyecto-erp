eba8f1b976e2ac81b9f4f3aef6536e8218fa6f69
Criscai964989 <cristiancaicedo68@gmail.com>
2026-01-21 17:16:00 -0500

    Corrigiendo URL del backend para producción

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import {
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell } from 'recharts';
 
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = window.location.origin;
 
 const fmt = (number) => new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 }).format(number || 0);
 
